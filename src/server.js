f8e58d967b6e023f01b4f3a5dea0183fefee195c
Objelisks <objelisks@gmail.com>
2020-10-30 10:59:05 -0700

    authorize

--- src/server.js
+++ src/server.js
@@ -49,31 +49,11 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
-/*
-objelisks@ancient-pebble:~/code/picramp$ PORT=4200 npm start
 
-> picramp@0.0.1 start /home/objelisks/code/picramp
-> node __sapper__/build
-
-Starting server on port 4200
-express-session deprecated undefined resave option; provide resave option __sapper__/build/server/server.js:8196:35
-express-session deprecated undefined saveUninitialized option; provide saveUninitialized option __sapper__/build/server/server.js:8196:35
-Warning: connect.session() MemoryStore is not
-designed for a production environment, as it will leak
-memory, and will not scale past a single process.
-Error: Failed to serialize session data: Cannot stringify arbitrary non-POJOs
-    at /home/objelisks/code/picramp/__sapper__/build/server/server.js:7779:31
-    at try_serialize (/home/objelisks/code/picramp/__sapper__/build/server/server.js:7871:13)
-    at /home/objelisks/code/picramp/__sapper__/build/server/server.js:7778:41
-    at Generator.next (<anonymous>)
-    at fulfilled (/home/objelisks/code/picramp/__sapper__/build/server/server.js:3217:58)
-
-*/
 express()
   .use(session({ secret: "fgsfds" }))
   .use(passport.initialize())
   .use(passport.session())
-  .get("/picramp/auth/redirect", passport.authenticate("mastodon"))
   .use((req, res, next) => {
     if (dev) {
       // cheating
@@ -85,8 +65,12 @@ express()
     "/picramp/login",
     passport.authenticate("mastodon", {
       scope: "read:accounts",
-      failureRedirect: "/",
+      failureRedirect: "/picramp",
     })
+  )
+  .get(
+    "/picramp/login/redirect",
+    passport.authorize("mastodon", { failureRedirect: "/picramp" })
   )
   .use("/picramp/upload", fileUpload)
   .use("/picramp/rest", store)
